1d09b397d418dadaf16c2cc8c2c4c4999bd1758d
Moriarty002 <43646155+Moriarty002@users.noreply.github.com>
2019-04-25 20:17:43 +0800

    1

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // TODO(you): Write the JavaScript necessary to complete the homework.
+function C_SoNS() {
 
+}
 // You can access the RESULTS_MAP from "constants.js" in this file since
 // "constants.js" has been included before "script.js" in index.html.
