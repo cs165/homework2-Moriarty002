bfbbfbe277077cc74a18e165a8095e44a7432af9
Moriarty002 <43646155+Moriarty002@users.noreply.github.com>
2019-04-25 23:58:27 +0800

    js_half

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 // TODO(you): Write the JavaScript necessary to complete the homework.
-function C_SoNS() {
+function C_SoNS(envent)
+{
+    x=event.currentTarget;
+    var i;
+    for(i=0;i<27;i++)
+    {
+        if(choice_h[i].dataset.questionId === x.dataset.questionId)
+        {
+            toNS(choice_h[i]);
+        }
+    }
+    toS(x);
+}
+function toS(space) //change div to selected
+{
+    space.style.background='#cfe3ff';
+    space.getElementsByClassName("checkbox")[0].src="images/checked.png";
+}
+function toNS(space) //change div to unselected
+{
+    space.style.background='#f4f4f4';
+    space.getElementsByClassName("checkbox")[0].src="images/unchecked.png";
+}
 
+const choice_h=document.querySelectorAll('section.choice-grid > div.container');
+for (const choice of choice_h)
+{
+    choice.addEventListener('click', C_SoNS );
 }
 // You can access the RESULTS_MAP from "constants.js" in this file since
 // "constants.js" has been included before "script.js" in index.html.
